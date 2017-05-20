ec7f0f2bfde977686671e2dc7c635840eaf591e8
Nikolay Aleschenko <nick130586@gmail.com>
2017-05-20 18:22:08 +0300

    Initial Commit

---
+++ LegacyProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LegacyProject
+//
+//  Created by Nikolay Aleshchenko on 5/20/17.
+//  Copyright © 2017 Nikolay Aleshchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LegacyProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LegacyProject
+//
+//  Created by Nikolay Aleshchenko on 5/20/17.
+//  Copyright © 2017 Nikolay Aleshchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
