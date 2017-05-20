59de607dd4280a2405f9e87439ba5b9fac8ed4c9
Nikolay Aleschenko <nick130586@gmail.com>
2017-05-20 18:51:14 +0300

    Implement legacy code call; remove unnecessary code

---
+++ LegacyProject/LegacyCallHandler.h
@@ -0,0 +1,15 @@
+//
+//  ObjectiveCHandler.h
+//  LegacyProject
+//
+//  Created by Nikolay Aleshchenko on 5/20/17.
+//  Copyright © 2017 Nikolay Aleshchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LegacyCallHandler : NSObject
+
+- (NSString*) textForLegacyCall;
+
+@end
---
+++ LegacyProject/LegacyCallHandler.m
@@ -0,0 +1,17 @@
+//
+//  ObjectiveCHandler.m
+//  LegacyProject
+//
+//  Created by Nikolay Aleshchenko on 5/20/17.
+//  Copyright © 2017 Nikolay Aleshchenko. All rights reserved.
+//
+
+#import "LegacyCallHandler.h"
+
+@implementation LegacyCallHandler
+
+- (NSString*) textForLegacyCall {
+    return @"Text from Objective-C class";
+}
+
+@end
